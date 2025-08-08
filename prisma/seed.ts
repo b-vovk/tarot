e86b8ca143ecbe12e85bb3eb2ecb826cdda6a8c2
Bogdan Vovk <me.vovk@gmail.com>
2025-08-08 13:37:37 +0100

    update from scratch

--- prisma/seed.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  console.log('Nothing to seed by default.');
-}
-main().finally(async () => prisma.$disconnect());
--- src/middleware.ts
+++
@@ -1,2 +0,0 @@
-export { auth as middleware } from 'next-auth/middleware';
-export const config = { matcher: ['/dashboard/:path*', '/history/:path*'] };
